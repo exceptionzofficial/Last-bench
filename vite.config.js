712107cd03a5544fa97e126ac3fcbd21ce3f6dc4
exceptionzofficial <exceptionzofficial@gmail.com>
2026-05-23 13:42:13 +0530

    build: Add multi-page Rollup input configuration for jobs.html in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,13 @@ export default defineConfig({
         secure: false,
       }
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        jobs: './jobs.html'
+      }
+    }
   }
 });
